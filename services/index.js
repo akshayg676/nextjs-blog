f477ae7f9a65603e3f70abc498415662e74e21ab
AkshayG-accruon <akshay@accruontechnologies.co.in>
2022-09-27 18:57:40 +0530

    complete recent post section

--- services/index.js
+++ services/index.js
@@ -56,3 +56,27 @@ export const getRecentPosts = async () => {
   const response = await request(graphqlAPI, query);
   return response.posts;
 };
+
+export const getSimilarPosts = async (categories, slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query, { slug, categories });
+
+  return result.posts;
+};
